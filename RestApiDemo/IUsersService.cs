80a253d9c901220d1b1d77964115c919ed712e5d
alice70106 <alice70106@gmail.com>
2021-11-02 09:55:54 +0800

    補上缺少的檔案

---
+++ RestApiDemo/IUsersService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestApiDemo
+{
+    interface IUsersService
+    {
+        public List<User> list{ get; set; }
+        public List<User> All();
+        public User Find(Guid id);
+
+        public User AddUser(JSONViewModel input);
+
+        public void DeleteUser(Guid id);
+       
+    }
+}
